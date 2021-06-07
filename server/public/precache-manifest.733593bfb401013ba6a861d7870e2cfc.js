de6305c5896d865dfd83283875ec0bd07020e123
Barath <barath6.99@gmail.com>
2021-06-07 22:39:09 +0530

    Server added

---
+++ server/public/precache-manifest.733593bfb401013ba6a861d7870e2cfc.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "675e013df3ba69c10b0674fb01d6fcc1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42cbb3bc07ddbbc8eee0",
+    "url": "/static/css/2.51a53583.chunk.css"
+  },
+  {
+    "revision": "2baf411aaf8d4b1f1f7e",
+    "url": "/static/css/main.b37d8f83.chunk.css"
+  },
+  {
+    "revision": "42cbb3bc07ddbbc8eee0",
+    "url": "/static/js/2.b8189422.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.b8189422.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2baf411aaf8d4b1f1f7e",
+    "url": "/static/js/main.c9707868.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  },
+  {
+    "revision": "a06bc802305d055c2b073c1bd9cbe1c2",
+    "url": "/static/media/exide150ah.a06bc802.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "6e4b5402b6c2d17f199d59c56425ff6c",
+    "url": "/static/media/logo.6e4b5402.jpg"
+  }
+]);
